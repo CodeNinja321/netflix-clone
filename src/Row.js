087a5d5737cea0e451a7b59e32db293d039131c2
Djóni Frank Strømsten <djonifrank@gmail.com>
2020-08-16 22:24:02 +0100

    created basic layout

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios';
+import requests from './requests';
+import './Row.css';
 
-const Row = () => {
+const base_url = 'https://image.tmdb.org/t/p/original';
+
+const Row = ({ title, fetchUrl }) => {
+    const [movies, setMovies] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+    
     return (
-        <div>
+        <div className='row'>
+            <h2>{ title }</h2>
             
+            <div className='row__posters'>
+                {movies.map(movie => (
+                    <img
+                        className='row__poster' 
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name} />
+                ))}
+            </div>
         </div>
     )
 }
